8b1bf63f53c4341a1ce52944e1f901647a5e2deb
Дом <ira-rostov80@mail.ru>
2022-08-31 22:16:41 +0300

    dog and cat and paper 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args)
-    {   var dog = 8;
+    {   var dog = 8.0;
         System.out.println(dog);
-        dog = dog + 4;
+        dog = dog - 3.5;
         System.out.println(dog);
         var cat = 3.6;
         System.out.println(cat);
-        cat = cat + 4;
+        cat = cat - 1.6;
         System.out.println(cat);
         var paper = 763789;
         System.out.println(paper);
-        paper = paper + 4;
+        paper = paper - 7639;
         System.out.println(paper);}
 }
