a3191b478947b61efa4e8bc1c69cf41043d7ddd9
Дом <ira-rostov80@mail.ru>
2022-09-01 16:23:01 +0300

    employees and hours

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,14 @@ public class Main {
         System.out.println ("Остаток от деления" + divisionDelay);
         var athleteWeight = (firstboxersWight + secondboxersWight) % divisionDelay;
         System.out.println("Разница деления" + athleteWeight);
+        var openingHours = 640.0;
+        var timeWorker = 8.0;
+        var totalEmployees = openingHours / timeWorker;
+        System.out.println("Всего работников в компании" + totalEmployees);
+        var morePeople = 94.0;
+        var totalPeople = totalEmployees + morePeople;
+        System.out.println("Всего людей" + totalPeople);
+        var divideHours = totalPeople * timeWorker;
+        System.out.println("Если в компании работает 174 человека, то всего часов может быть поделено между сотрудниками" + divideHours);
     }
-}
+    }
