a90e1b598b8ca2f02105a3085f84f40515ddf614
Дом <ira-rostov80@mail.ru>
2022-09-01 10:49:24 +0300

    friend and frog

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args)
-    {   var dog = 8.0;
-        System.out.println(dog);
-        dog = dog - 3.5;
-        System.out.println(dog);
-        var cat = 3.6;
-        System.out.println(cat);
-        cat = cat - 1.6;
-        System.out.println(cat);
-        var paper = 763789;
-        System.out.println(paper);
-        paper = paper - 7639;
-        System.out.println(paper);}
+    {   var friend = 19.0;
+        System.out.println(friend);
+        friend = friend * 2.0;
+        System.out.println(friend);
+        friend = friend / 7.0;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10.0;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4.0;
+        System.out.println(frog);}
 }
