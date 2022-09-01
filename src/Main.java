d80c987c06ed4f77c49b92c6875d080c540d89df
Дом <ira-rostov80@mail.ru>
2022-09-01 14:53:14 +0300

    fighters

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 public class Main {
     public static void main(String[] args)
-    {   var friend = 19.0;
-        System.out.println(friend);
-        friend = friend * 2.0;
-        System.out.println(friend);
-        friend = friend / 7.0;
-        System.out.println(friend);
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10.0;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4.0;
-        System.out.println(frog);}
+    {   var firstboxersWight = 78.2;
+        var secondboxersWight = 82.7;
+        var fighters = firstboxersWight + secondboxersWight;
+        System.out.println("Общий вес бойцов" + fighters);
+        var weightDifference =  secondboxersWight - firstboxersWight;
+        System.out.println("Разница веса" + weightDifference);
+        var divisionDelay = secondboxersWight / firstboxersWight;
+        System.out.println ("Остаток от деления" + divisionDelay);
+        var athleteWeight = (firstboxersWight + secondboxersWight) % divisionDelay;
+        System.out.println("Разница деления" + athleteWeight);
+    }
 }
